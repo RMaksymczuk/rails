e088cf688057f2fed8135955d5f96cbb8ae7bed8
Rafal.Maksymczuk <rafal.maksymczuk@amberbit.com>
2013-12-23 10:30:47 +0100

    Fix#2 empty array in DatabaseStatements to_sql method

--- activerecord/lib/active_record/connection_adapters/abstract/database_statements.rb
+++ activerecord/lib/active_record/connection_adapters/abstract/database_statements.rb
@@ -9,7 +9,7 @@ module ActiveRecord
       # Converts an arel AST to SQL
       def to_sql(arel, binds = [])
         if arel.respond_to?(:ast)
-          binds = binds.dup
+#          binds = binds.dup
           visitor.accept(arel.ast) do
             binds = [""] if binds.empty?
             quote(*binds.shift.reverse)
